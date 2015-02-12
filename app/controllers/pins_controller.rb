6acacd40b6acdba5e6ad4716904fa4e0a4f8e4fa
Jesus Marin <jesusangelm@gmail.com>
2015-02-12 10:02:40 -0430

    add pin controler model and basic views

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,51 @@
+class PinsController < ApplicationController
+  before_action :search_pin, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all.order("created_at DESC")
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def show
+  end
+
+  def edit
+    
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Pin creado."
+    else
+      render "new"
+    end
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin actualizado"
+    else
+      redirect_to "edit"
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def search_pin
+    @pin = Pin.find(params[:id])
+  end
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+end
