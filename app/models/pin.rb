cf532d91e908a94f512180eabdfd5fbd5d29a5a6
Jesus Marin <jesusangelm@gmail.com>
2015-02-19 14:53:26 -0430

    add some validations for category and pin models

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,6 +4,8 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
+  validates :title, :description, :user_id, :category_id, presence: true
+
   has_attached_file :image, styles: { medium: "300x300"}
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
